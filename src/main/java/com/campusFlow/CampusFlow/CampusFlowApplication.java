4540a176f5d86bf23ae157aaf0924578eec562eb
Felipe Buri <152114793+Swichxx@users.noreply.github.com>
2025-09-10 16:54:39 -0300

    Update CampusFlowApplication.java

--- src/main/java/com/campusFlow/CampusFlow/CampusFlowApplication.java
+++ src/main/java/com/campusFlow/CampusFlow/CampusFlowApplication.java
@@ -2,11 +2,20 @@ package com.campusFlow.CampusFlow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class CampusFlowApplication {
 
 	public static void main(String[] args) {
+
+		// Carrega o .env da raiz do projeto
+	//	Dotenv dotenv = Dotenv.load();
+
+		// Pega a variável ENDPOINT_KEY
+	//	String endpointKey = dotenv.get("ENDPOINT_KEY");
+		//System.out.println("Chave do endpoint: " + endpointKey);
+		
 		SpringApplication.run(CampusFlowApplication.class, args);
 	}
 
